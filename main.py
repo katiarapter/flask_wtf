edeade5d2f1359adaec92a7bb9f83201132b6c1b
Katiapater <katiasafronova08@yandex.ru>
2023-03-01 23:28:08 +0300

    Тренировка в полете

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/index/<title>')
 def index(title):
     return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    if prof in ['строитель', 'инженер']:
+        return render_template('training.html', prof='Инженерные тренажеры', ship_image="space2.png")
+    else:
+        return render_template('training.html', prof='Научные симуляторы', ship_image="space1.png")
 
 
 if __name__ == '__main__':
