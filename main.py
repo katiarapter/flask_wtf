93cb31f1b59614e93bdca95eaa19d3645824dbe9
Katiapater <katiasafronova08@yandex.ru>
2023-03-02 00:03:43 +0300

    Список профессий

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ def index(title):
     return render_template('base.html', title=title)
 
 
-@app.route('/training/<prof>')
-def training(prof):
-    if prof in ['строитель', 'инженер']:
-        return render_template('training.html', prof='Инженерные тренажеры', ship_image="space2.png")
+@app.route('/training/<profession>')
+def training(profession):
+    if profession in ['строитель', 'инженер']:
+        return render_template('training.html', profession='Инженерные тренажеры', ship_image="space2.png")
     else:
-        return render_template('training.html', prof='Научные симуляторы', ship_image="space1.png")
+        return render_template('training.html', profession='Научные симуляторы', ship_image="space1.png")
+
+
+@app.route('/list_prof/<list_index>')
+def list_prof(list_index):
+    return render_template('list_prof.html', list_index=list_index)
 
 
 if __name__ == '__main__':
